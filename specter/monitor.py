f3dd39bb92e30a6387a9e99135cdee5252b4ca7f
natefduncan <natefduncan@gmail.com>
2021-05-20 10:43:06 -0500

    monitor working

--- specter/monitor.py
+++ specter/monitor.py
@@ -16,37 +16,86 @@ def get_last_modified(path):
     dt = datetime.datetime.fromtimestamp(ts)
     return dt
 
+def get_last_accessed(path):
+    ts = os.path.getatime(path)
+    dt = datetime.datetime.fromtimestamp(ts)
+    return dt
+
 def saved_files(files, SAVES):
     output = []
     for file in files:
         if file not in SAVES["files"]:
             output.append(file)
         else:
-            if get_last_modified(file).timestamp() > SAVES["last_saved"]:
+            if get_last_modified(file).timestamp() > SAVES["last_checked"]:
                 output.append(file)
     return output
 
+def opened_files(files, SAVES):
+    output = []
+    for file in files:
+        if get_last_accessed(file).timestamp() > SAVES["last_checked"]:
+            output.append(file)
+    return output   
+
 def dump_saves(files):
     with open("saves.json", "w") as f:
-        json.dump({"last_saved" : datetime.datetime.now().timestamp(), "files" : files}, f)
+        json.dump({"last_checked" : datetime.datetime.now().timestamp(), "files" : files}, f)
 
-def save_loop(path, wait=5, trigger_func=None, *args, **kwargs):
-    SAVES = load_saves()
-    while True: 
-        try:
-            files = get_all_files(path, ignore=[".DS_Store", "/data", "/.git", "sonic.cfg", ".gitignore", "\\data", "\\.git"])
-            saved = saved_files(files, SAVES)
-            if len(saved) > 0:
-                dump_saves(files)
-                if trigger_func:
-                    trigger_func(*args, **kwargs)
-                SAVES = load_saves()
-            time.sleep(wait)
-        except KeyboardInterrupt:
-            break
+class DirLoop:
+
+    def __init__(self, path, wait=5):
+        self.path = path
+        self.wait = wait
+
+    def set_save_func(self, function, *args, **kwargs):
+        self.save_func = function
+        self.save_args = args
+        self.save_kwargs = kwargs
+    
+    def set_open_func(self, function, *args, **kwargs):
+        self.open_func = function
+        self.open_args = args
+        self.open_kwargs = kwargs
+
+    def trigger_save(self):
+        self.save_func(*self.save_args, **self.save_kwargs)
+
+    def trigger_open(self):
+        self.open_func(*self.open_args, **self.open_kwargs)
+
+    def start(self):
+        SAVES = load_saves()
+        while True: 
+            try:
+                files = get_all_files(self.path, ignore=[".DS_Store", "/data", "/.git", "sonic.cfg", ".gitignore", "\\data", "\\.git"])
+                saved = saved_files(files, SAVES)
+                opened = opened_files(files, SAVES)
+                opened = [i for i in opened if i not in saved]
+                #SAVED
+                if len(saved) > 0:
+                    if self.save_func:
+                        self.trigger_save()
+                #OPENED
+                if len(opened) > 0:
+                    if self.open_func:
+                        self.trigger_open()
+                
+                if len(opened) > 0 or len(saved) > 0:
+                    dump_saves(files)
+                    SAVES = load_saves()
+                time.sleep(self.wait)
+            except KeyboardInterrupt:
+                break    
 
 if __name__=="__main__":
     def notify(x):
         print(x)
+
     home_dir = Path(os.path.expanduser('~'))
-    save_loop(f"{str(home_dir)}/Documents/Notes", wait=5, trigger_func=notify, x="SAVED")
+    dir_path = f"{str(home_dir)}/Documents/Notes"
+
+    l = DirLoop(path=dir_path)
+    l.set_save_func(notify, x="SAVED")
+    l.set_open_func(notify, x="OPENED")
+    l.start()
